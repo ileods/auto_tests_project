0c4c843326997ecd63b3c70d320b00d987ea196c
ileods <daryaisachenkos@gmail.com>
2023-11-22 13:39:32 +0300

    update base page

--- pages/locators.py
+++ pages/locators.py
@@ -9,10 +9,11 @@ class ProductPageLocators:
     CONFIRMATION_MESSAGE2 = (By.CSS_SELECTOR, '.alert:last-child strong')
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -3,11 +3,17 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    def add_to_basket(self):
+    def add_to_basket_promo(self):
         self.should_be_promo_url()
         self.should_be_basket_button()
         self.browser.find_element(*ProductPageLocators.BASKET_BTN).click()
         self.solve_quiz_and_get_code()
+        self.should_be_confirmation()
+        self.should_be_right_price()
+
+    def add_to_basket(self):
+        self.should_be_basket_button()
+        self.browser.find_element(*ProductPageLocators.BASKET_BTN).click()
         self.should_be_confirmation()
         self.should_be_right_price()
 
@@ -25,3 +31,10 @@ class ProductPage(BasePage):
         assert (self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
                 == self.browser.find_element(*ProductPageLocators.CONFIRMATION_MESSAGE2).text), "Price is not right"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.CONFIRMATION_MESSAGE1), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.CONFIRMATION_MESSAGE1), \
+            "Success message is presented, but should not be disappeared"
